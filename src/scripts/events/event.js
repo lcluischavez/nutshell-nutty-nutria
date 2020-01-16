a722f0b72907270733d1530ad8079d3f5c8107ff
Mitali Vasisht <vamdev37@gmail.com>
2020-01-16 09:46:22 -0600

    merge master

---
+++ src/scripts/events/event.js
@@ -0,0 +1,10 @@
+const eventComponenet = (flower) =>{
+    return `
+      <section class="eventCard">
+      <h2 class="flowerName">${event.commonName}</h2>
+      <p class="flowerColor">${flower.color}</p>
+      </section>
+    `
+  }
+  
+  export default flowerComponent
