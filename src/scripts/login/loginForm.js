5564bbc8b03b9d7ba2bacd37a830e6af9142ff66
Case Scally <casescally@gmail.com>
2020-01-17 10:29:05 -0600

    login components added

--- src/scripts/login/loginForm.js
+++ src/scripts/login/loginForm.js
@@ -1,11 +1,6 @@
-const loginTarget = document.getElementById(loginDiv);
-
-
 export const loginForm = () => {
 
-    return loginTarget.innerHTML=
-                    
-    `<div class="loginPage">
+    return `<div class="loginPage">
 
     <h1>Nutshell Login Page</h1>
 
@@ -16,14 +11,7 @@ export const loginForm = () => {
         <input type="reset" value="Cancel"/>
     </form>
 
-    <form name="registration">
-        First Name: <input type="text" id="firstName"/>
-        Last Name: <input type="text" id="lastName"/>
-        Email Address: <input type="text" id="emailAddress"/>
-        Password: <input type=password" name="pswrd" id="registrationPassword"/>
-        Confirm Password: <input type="password" name="pswrd" id="confirmPassword"/>
-        <input type="button" id="submitLogin" id="submitRegistration" value="Register"/>
-    </form>
-
     </div>
-`}
+`
+}
+
