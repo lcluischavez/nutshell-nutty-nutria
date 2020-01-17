98e1be9774da3627d55f4fd1620f22f30b7d2228
lcluischavez <lcluischavezz@gmail.com>
2020-01-17 09:18:28 -0600

    Tasks and Articles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,17 @@ import { getUsers } from "./users/userProvider.js";
 import TaskList from "./task/taskList.js";
 // import { getFriends } from "./friends/friendDataProvider";
 // import { getEvents } from "./events/eventDataProvider";
-// import { getArticles } from "./articles/articleDataProvider";
+import { getArticles } from "./articles/articleDataProvider.js";
+import ArticleList from "./articles/articleList.js";
 
+
+ArticleList
 
 getUsers()
+    .then(getArticles)
+    .then(ArticleList)
     .then(getTasks)
     .then(TaskList)
+
 
 
