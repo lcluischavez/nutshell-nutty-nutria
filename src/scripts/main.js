ab12bab27008bd934ef6e7d2b3fe4400998cb81d
philipDmartin <philipnathan788@gmail.com>
2020-01-17 12:03:31 -0600

    new master

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,21 @@
-//import { getMessages } from "./message/messageDataProvider.js";
+import { getMessages } from "./message/messageDataProvider.js";
 import { getTasks } from "./task/taskDataProvider.js";
 import { getUsers } from "./users/userProvider.js";
-//import MessageList from "./message/messageList.js";
+import messageListComponent from "./message/messageList.js";
 import TaskList from "./task/taskList.js";
 // import { getFriends } from "./friends/friendDataProvider";
-import { getEvents } from "./events/eventDataProvider.js";
+// import { getEvents } from "./events/eventDataProvider.js";
 import { getArticles } from "./articles/articleDataProvider.js";
 import ArticleList from "./articles/articleList.js";
-import EventList from "./events/eventList.js";
-
+// import eventListComponent from "./events/eventList.js";
 
 getUsers()
     .then(getArticles)
     .then(ArticleList)
     .then(getTasks)
     .then(TaskList)
-    .then(getEvents)
-    .then(EventList)
-
-
+    .then(getMessages)
+    .then(messageListComponent)
+    // .then(getEvents)
+    // .then(eventListComponent)
+    
