069ddbabaa87982b0166f54784f3596269def209
philipDmartin <philipnathan788@gmail.com>
2020-01-20 12:09:26 -0600

    done messages

--- src/scripts/message/message.js
+++ src/scripts/message/message.js
@@ -1,15 +1,15 @@
-const messagesComponent = (messages, users) => {
-    return `
-        <section class="message">
-            <div>
-                <h3> ${messages.message} </h3>
-            </div>
-            <div>
-                <button id="messageDelete--${messages.id}">Delete</button>
-                <button id="messageEdit--${messages.id}">Edit</button>
-            </div>
-        </section>
-    `
-}
+// const messagesComponent = (messages, users) => {
+//     return `
+//         <section class="message">
+//             <div>
+//                 <h3> ${messages.message} </h3>
+//             </div>
+//             <div>
+//                 <button id="messageDelete--${messages.id}">Delete</button>
+//                 <button id="messageEdit--${messages.id}">Edit</button>
+//             </div>
+//         </section>
+//     `
+// }
 
-export default messagesComponent
+// export default messagesComponent
