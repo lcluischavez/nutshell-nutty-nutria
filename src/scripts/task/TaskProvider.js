7b5308221bbdbc9ddc191e3541e19097ff9c477f
philipDmartin <philipnathan788@gmail.com>
2020-01-21 15:20:40 -0600

    fixed

--- src/scripts/task/TaskProvider.js
+++ src/scripts/task/TaskProvider.js
@@ -44,10 +44,3 @@ export const getTasks = () => {
         })
 }
 
-// let tasks = []
-
-// export const useTasks = () => tasks.slice()
-
-// export const getTasks = () => fetch("http://localhost:3000/tasks")
-//     .then(res => res.json())
-//     .then(parsedTasks => tasks = parsedTasks)
